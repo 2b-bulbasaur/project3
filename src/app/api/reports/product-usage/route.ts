5108d29b609925b490338e899d76c99a6b59602e
anyi8 <123267865+anyi8@users.noreply.github.com>
2024-10-27 23:52:21 -0500

    count ingredients in meal orders

--- src/app/api/reports/product-usage/route.ts
+++ src/app/api/reports/product-usage/route.ts
@@ -3,10 +3,6 @@ import { getTransactions } from '@/lib/transactions';
 import { getIngredientsForMenuItem } from '@/lib/menu';
 import { getMealOrderById } from '@/lib/orders';
 
-type HourlyCount = {
-    [key: string]: number;
-};
-
 export async function POST(request: Request) {
     try {
         const { input1, input2 } = await request.json();
@@ -17,20 +13,34 @@ export async function POST(request: Request) {
         const endDate = new Date(year2, month2-1, day2, 23, 59, 59);
 
         const transactions = await getTransactions();
-        // transactions.forEach(transaction => {
-        //     const transactionDate = new Date(transaction.date);
+        const ingredientMap = new Map<number, number>();
 
-        //     if (transactionDate >= startDate && transactionDate <= endDate) {
-        //         const mea
-        //     }
-        // })
+        transactions.forEach(async transaction => {
+            const transactionDate = new Date(transaction.date);
 
+            if (transactionDate >= startDate && transactionDate <= endDate) {
+                const orderId = transaction.id;
+                const mealsOrdered = await getMealOrderById(orderId);
 
-        
+                mealsOrdered.forEach(async order => {
+                    if (order.entree1 != null) {
+                        const ingredients = await getIngredientsForMenuItem(order.entree1);
+                        ingredients.forEach(ingredient => {
+                            ingredientMap.set(ingredient.id, (ingredientMap.get(ingredient.id) || 0) + 1);
+                        });
+                    }
+                    
+                });
+            }
+        })
+
+    
+        return NextResponse.json(ingredientMap);    
     }
-    catch {
+    catch (error) {
+        console.error('Error in generateXReport:', error);
         return NextResponse.json(
-            { error: 'Failed to generate Product Usage Report'},
+            { error: 'Failed to generate X Report', details: error instanceof Error ? error.message : 'Unknown error' },
             { status: 500 }
         );
     }
